e9917688a48445ca84d88e92577e12d262656db1
Cédric Martin <cedmart1@gmail.com>
2022-05-25 15:16:15 +0200

    Reverted workgroup size

--- h5dmd/js/renderers/DummyRenderer.mjs
+++ h5dmd/js/renderers/DummyRenderer.mjs
@@ -44,7 +44,7 @@ class DummyRenderer {
                             @group(0) @binding(1) var<storage,write> outputPixels: Image;
                             
                             @stage(compute)
-                            @workgroup_size(64)
+                            @workgroup_size(1)
                             fn main (@builtin(global_invocation_id) global_id: vec3<u32>) {
                                 let index : u32 = global_id.x + global_id.y * ${that.#width}u;
                                 let pixelColor : u32 = inputPixels.rgba[index];
