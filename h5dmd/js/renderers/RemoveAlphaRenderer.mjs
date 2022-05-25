e1fb1db1eb3071effa0a46add337014ccac77f28
Cédric Martin <cedmart1@gmail.com>
2022-05-25 15:01:08 +0200

    WTF

--- h5dmd/js/renderers/RemoveAlphaRenderer.mjs
+++ h5dmd/js/renderers/RemoveAlphaRenderer.mjs
@@ -46,7 +46,7 @@ class RemoveAlphaRenderer {
                             @group(0) @binding(1) var<storage,write> outputPixels: Image;
 
                             @stage(compute)
-                            @workgroup_size(1)
+                            @workgroup_size(64)
                             fn main (@builtin(global_invocation_id) global_id: vec3<u32>) {
                                 let index : u32 = global_id.x + global_id.y * ${that.#width}u;
 
@@ -173,7 +173,7 @@ class RemoveAlphaRenderer {
 
             passEncoder.setPipeline(computePipeline);
             passEncoder.setBindGroup(0, bindGroup);
-            //passEncoder.dispatch(that.#width, that.#height);
+            passEncoder.dispatch(that.#width, that.#height);
             passEncoder.end();
 
             commandEncoder.copyBufferToBuffer(gpuTempBuffer, 0, gpuOutputBuffer, 0, that.#bufferByteLength);
