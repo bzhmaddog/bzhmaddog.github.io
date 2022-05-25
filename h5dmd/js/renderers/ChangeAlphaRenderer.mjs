b9bb343f35210dd39217fa4c6e2c2caa9c6c95f3
Cédric Martin <cedmart1@gmail.com>
2022-05-25 14:59:44 +0200

    WTF

--- h5dmd/js/renderers/ChangeAlphaRenderer.mjs
+++ h5dmd/js/renderers/ChangeAlphaRenderer.mjs
@@ -52,7 +52,7 @@ class ChangeAlphaRenderer {
                             @group(0) @binding(2) var<uniform> uniforms : UBO;                            
 
                             @stage(compute)
-                            @workgroup_size(1)
+                            @workgroup_size(64)
                             fn main (@builtin(global_invocation_id) global_id: vec3<u32>) {
                                 let index : u32 = global_id.x + global_id.y * ${that.#width}u;
                                 let pixelColor : u32 = inputPixels.rgba[index];
@@ -216,7 +216,7 @@ class ChangeAlphaRenderer {
 
             passEncoder.setPipeline(computePipeline);
             passEncoder.setBindGroup(0, bindGroup);
-            //passEncoder.dispatchWorkgroups(that.#width, that.#height);
+            passEncoder.dispatchWorkgroups(that.#width, that.#height);
             passEncoder.end();
 
             commandEncoder.copyBufferToBuffer(gpuTempBuffer, 0, gpuOutputBuffer, 0, that.#bufferByteLength);
